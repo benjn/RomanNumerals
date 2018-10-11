f4f61da6c8699f664ed77a71263b40482757c790
bmustone <bmustone@W10-127.student.yo.oulu.fi>
2018-10-11 17:13:02 +0300

    Assignment Finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,119 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		if(verifyRoman(romanNum)) {
+			List<String> romanNums = splitString(romanNum);
+			Map<String, Integer> numeralMap = makeNumMap();
+			
+			int asInteger = 0;
+			for(int i=0; i<romanNums.size(); i++) {
+				if(i == romanNums.size()-1) {
+					asInteger = asInteger + numeralMap.get(romanNums.get(i));
+				}else if(numeralMap.get(romanNums.get(i)) < numeralMap.get(romanNums.get(i+1))) {
+					asInteger = asInteger - numeralMap.get(romanNums.get(i));
+				}else {
+					asInteger = asInteger + numeralMap.get(romanNums.get(i));
+				}
+			}
+					
+			return asInteger;
+		}else {
+			return 0;
+		}
+	}
+	
+	List<String> splitString (String toSplit){
+		List<String> splitList = new ArrayList<String>();
+		for(int i=0; i<toSplit.length(); i++) {
+			splitList.add(Character.toString(toSplit.charAt(i)));
+		}
 		
+		return splitList;
+	}
+	
+	Map<String, Integer> makeNumMap(){
+		Map<String, Integer> numeralMap = new HashMap<String, Integer>();
+		numeralMap.put("I", 1);
+		numeralMap.put("V", 5);
+		numeralMap.put("X", 10);
+		numeralMap.put("L", 50);
+		numeralMap.put("C", 100);
+		numeralMap.put("D", 500);
+		numeralMap.put("M", 1000);
+		
+		return numeralMap;
+	}
+	
+	boolean verifyRoman(String toVerify) {
+		List<String> asList = splitString(toVerify);
+		Map<String, Integer> numeralMap = makeNumMap();
+		
+		//Not a Roman numeral
+		if(!toVerify.matches("^[IVXLCDM]*$")) {
+			return false;
+		}
+		
+		for(int i=0; i<asList.size(); i++) {
+			//I, X, C and M can be repeated at most 3 times in a row
+			if((asList.get(i).equals("I")) || (asList.get(i).equals("X")) || (asList.get(i).equals("C")) || (asList.get(i).equals("M"))) {
+				if(i >= asList.size()-3) {
+					//no need to do anything
+				}else if((asList.get(i).equals(asList.get(i+1))) && (asList.get(i).equals(asList.get(i+2))) && (asList.get(i).equals(asList.get(i+3)))){
+					return false;
+				}
+			}
+			
+			//V, L and D can never be repeated
+			if((asList.get(i).equals("V")) || (asList.get(i).equals("L")) || (asList.get(i).equals("D"))) {
+				if(i >= asList.size()-1) {
+					//no need to do anything
+				}else if(asList.get(i).equals(asList.get(i+1))){
+					return false;
+				}
+			}
+			
+			//The "1" symbols (I, X and C) can only be subtracted from the 2 next highest values
+			if((asList.get(i).equals("I")) || (asList.get(i).equals("X")) || (asList.get(i).equals("C"))) {
+				if(i >= asList.size()-1) {
+					//no need to do anything
+				}else if(numeralMap.get(asList.get(i)) < numeralMap.get(asList.get(i+1))) {
+					if(asList.get(i).equals("I")){
+						if((!asList.get(i+1).equals("V")) && (!asList.get(i+1).equals("X"))) {
+							return false;
+						}
+					}else if(asList.get(i).equals("X")){
+						if((!asList.get(i+1).equals("L")) && (!asList.get(i+1).equals("C"))) {
+							return false;
+						}
+					}else if(asList.get(i).equals("C")){
+						if((!asList.get(i+1).equals("D")) && (!asList.get(i+1).equals("M"))) {
+							return false;
+						}
+					}
+				}
+			}
+			
+			//Only one subtraction can be made per numeral
+			if(i >= asList.size()-2) {
+				//no need to do anything
+			}else if((asList.get(i).equals(asList.get(i+1))) && (numeralMap.get(asList.get(i)) < numeralMap.get(asList.get(i+2)))){
+				return false;
+			}
+			
+			//The "5" symbols (V, L and D) can never be subtracted
+			if((asList.get(i).equals("V")) || (asList.get(i).equals("L")) || (asList.get(i).equals("D"))) {
+				if(i >= asList.size()-1) {
+					//no need to do anything
+				}else if(numeralMap.get(asList.get(i)) < numeralMap.get(asList.get(i+1))){
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,206 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConvertToIntegers_1() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		int result = 0;
+						
+		// Act
+		result = numerals.convertToInteger("I");
+						
+		// Assert
+		assertEquals(1, result);
 	}
-
+	
+	@Test
+	public void testConvertToIntegers_500() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		int result = 0;
+						
+		// Act
+		result = numerals.convertToInteger("D");
+						
+		// Assert
+		assertEquals(500, result);
+	}
+	
+	@Test
+	public void testConvertToIntegers_3() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		int result = 0;
+						
+		// Act
+		result = numerals.convertToInteger("III");
+						
+		// Assert
+		assertEquals(3, result);
+	}
+	
+	@Test
+	public void testConvertToIntegers_4() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		int result = 0;
+						
+		// Act
+		result = numerals.convertToInteger("IV");
+						
+		// Assert
+		assertEquals(4, result);
+	}
+	
+	@Test
+	public void testConvertToIntegers_990() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		int result = 0;
+						
+		// Act
+		result = numerals.convertToInteger("CMXC");
+						
+		// Assert
+		assertEquals(990, result);
+	}
+	
+	@Test
+	public void testSplitString_IX() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		List<String> result = new ArrayList<String>();
+						
+		// Act
+		result = numerals.splitString("IX");
+						
+		// Assert
+		assertEquals("X", result.get(1));
+	}
+	
+	@Test
+	public void testVerifyRoman_InvalidVV() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		boolean result = true;
+						
+		// Act
+		result = numerals.verifyRoman("VV");
+						
+		// Assert
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testVerifyRoman_InvalidXXXX() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		boolean result = true;
+						
+		// Act
+		result = numerals.verifyRoman("XXXX");
+						
+		// Assert
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testMakeNumMap() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		Map<String, Integer> result = new HashMap<String, Integer>();
+						
+		// Act
+		result = numerals.makeNumMap();
+						
+		// Assert
+		assertEquals(Integer.valueOf(10), result.get("X"));
+	}
+	
+	@Test
+	public void testVerifyRoman_InvalidVX() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		boolean result = true;
+						
+		// Act
+		result = numerals.verifyRoman("VX");
+						
+		// Assert
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testVerifyRoman_InvalidIL() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		boolean result = true;
+						
+		// Act
+		result = numerals.verifyRoman("IL");
+						
+		// Assert
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testVerifyRoman_InvalidXXC() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		boolean result = true;
+						
+		// Act
+		result = numerals.verifyRoman("XXC");
+						
+		// Assert
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testConvertToIntegers_1984() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		int result = 0;
+						
+		// Act
+		result = numerals.convertToInteger("MCMLXXXIV");
+						
+		// Assert
+		assertEquals(1984, result);
+	}
+	
+	@Test
+	public void testVerifyRoman_InvalidNumeral() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		boolean result = true;
+						
+		// Act
+		result = numerals.verifyRoman("Hello");
+						
+		// Assert
+		assertEquals(false, result);
+	}
+	
+	@Test
+	public void testConvertToIntegers_InvalidNumeral() {
+		// Arrange
+		RomanNumerals numerals = new RomanNumerals();
+		int result = 0;
+						
+		// Act
+		result = numerals.convertToInteger("Hello");
+						
+		// Assert
+		assertEquals(0, result);
+	}
 }
